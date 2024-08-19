d8b3efa39652d190f731ef4be9934ad8005cfb1e
thawatchai_m <demonridercos@gmail.com>
2024-08-19 22:19:01 +0700

    edit login page

--- lib/login/loginPage.dart
+++ lib/login/loginPage.dart
@@ -16,209 +16,211 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(height: 20,),
-            Container(
-              height: 99,
-              width: 87,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/icons/Frame 61.png'),
-                  fit: BoxFit.fill, // ปรับแต่งการแสดงผลของภาพตามความต้องการ
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 20,),
+              Container(
+                height: 99,
+                width: 87,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/icons/Frame 61.png'),
+                    fit: BoxFit.fill, // ปรับแต่งการแสดงผลของภาพตามความต้องการ
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 30,),
-            Container(
-              height: 43.91,
-              width: 171.6,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/icons/Frame 76.png'),
-                  fit: BoxFit.fill, // ปรับแต่งการแสดงผลของภาพตามความต้องการ
+              SizedBox(height: 30,),
+              Container(
+                height: 43.91,
+                width: 171.6,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/icons/Frame 76.png'),
+                    fit: BoxFit.fill, // ปรับแต่งการแสดงผลของภาพตามความต้องการ
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 40,),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.person_2_outlined),
-                labelText: 'Email',
-                labelStyle: TextStyle(fontWeight: FontWeight.bold),
+              SizedBox(height: 40,),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.person_2_outlined),
+                  labelText: 'Email',
+                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password', // Hint text
-                prefixIcon: Icon(Icons.lock_outline),
-                suffixIcon: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(
-                      onPressed: () {
-
-                      },
-                      icon: Icon(Icons.remove_red_eye_outlined),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        
-                      },
-                      child: Text(
-                        'ลืมรหัสผ่าน?',
-                        style: TextStyle(
-                          color: Colors.blue[500],
-                          fontWeight: FontWeight.bold,
+              const SizedBox(height: 20),
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password', // Hint text
+                  prefixIcon: Icon(Icons.lock_outline),
+                  suffixIcon: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+        
+                        },
+                        icon: Icon(Icons.remove_red_eye_outlined),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          
+                        },
+                        child: Text(
+                          'ลืมรหัสผ่าน?',
+                          style: TextStyle(
+                            color: Colors.blue[500],
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
+                    ],
+                  ),
+                ),
+                obscureText: true,
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => registerpage()),
+                    );
+                  },
+                  child: Text(
+                    'ลงทะเบียนผู้ใช้ใหม่',
+                    style: TextStyle(
+                      color: Colors.blue[500],
+                      fontWeight: FontWeight.bold,
                     ),
-                  ],
+                    textAlign: TextAlign.right,
+                  ),
                 ),
               ),
-              obscureText: true,
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
+              const SizedBox(height: 15),
+              ElevatedButton(
                 onPressed: () {
-                  Navigator.push(
+                  Navigator.pushReplacement(
                     context,
-                    MaterialPageRoute(builder: (context) => registerpage()),
+                    MaterialPageRoute(builder: (context) => FirstPage()),
                   );
                 },
-                child: Text(
-                  'ลงทะเบียนผู้ใช้ใหม่',
-                  style: TextStyle(
-                    color: Colors.blue[500],
-                    fontWeight: FontWeight.bold,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 219, 18, 4),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 135, vertical: 15), // การเว้นระยะในปุ่ม
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    // มุมโค้งของปุ่ม
                   ),
-                  textAlign: TextAlign.right,
+                ),
+                child: const Text(
+                  'เข้าสู่ระบบ',
+                  style: TextStyle(
+                      fontSize: 20, color: Color.fromARGB(255, 255, 255, 255)),
                 ),
               ),
-            ),
-            const SizedBox(height: 15),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => FirstPage()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 219, 18, 4),
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 135, vertical: 15), // การเว้นระยะในปุ่ม
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  // มุมโค้งของปุ่ม
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'หรือ',
+                style: TextStyle(fontSize: 15),
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              ElevatedButton(
+                onPressed: () {
+        
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 100, vertical: 15), // การเว้นระยะในปุ่ม
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    // มุมโค้งของปุ่ม
+                  ),
+                ),
+                child: const Text(
+                  'ดำเนินต่อด้วยบัญชี Google',
+                  style: TextStyle(fontSize: 15, color: Colors.black),
                 ),
               ),
-              child: const Text(
-                'เข้าสู่ระบบ',
-                style: TextStyle(
-                    fontSize: 20, color: Color.fromARGB(255, 255, 255, 255)),
+              SizedBox(
+                height: 10,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'หรือ',
-              style: TextStyle(fontSize: 15),
-            ),
-            SizedBox(
-              height: 35,
-            ),
-            ElevatedButton(
-              onPressed: () {
-
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 100, vertical: 15), // การเว้นระยะในปุ่ม
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  // มุมโค้งของปุ่ม
+              ElevatedButton(
+                onPressed: () {
+        
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 93, vertical: 15), // การเว้นระยะในปุ่ม
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    // มุมโค้งของปุ่ม
+                  ),
+                ),
+                child: const Text(
+                  'ดำเนินต่อด้วยบัญชี Facebook',
+                  style: TextStyle(fontSize: 15, color: Colors.black),
                 ),
               ),
-              child: const Text(
-                'ดำเนินต่อด้วยบัญชี Google',
-                style: TextStyle(fontSize: 15, color: Colors.black),
+              SizedBox(
+                height: 10,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              onPressed: () {
-
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 93, vertical: 15), // การเว้นระยะในปุ่ม
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  // มุมโค้งของปุ่ม
+              ElevatedButton(
+                onPressed: () {
+                 
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 110, vertical: 15), // การเว้นระยะในปุ่ม
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    // มุมโค้งของปุ่ม
+                  ),
+                ),
+                child: const Text(
+                  'ดำเนินต่อด้วยบัญชี Line',
+                  style: TextStyle(fontSize: 15, color: Colors.black),
                 ),
               ),
-              child: const Text(
-                'ดำเนินต่อด้วยบัญชี Facebook',
-                style: TextStyle(fontSize: 15, color: Colors.black),
+              SizedBox(
+                height: 10,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              onPressed: () {
-               
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 110, vertical: 15), // การเว้นระยะในปุ่ม
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  // มุมโค้งของปุ่ม
+              ElevatedButton(
+                onPressed: () {
+                  
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 105, vertical: 15), // การเว้นระยะในปุ่ม
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: const Text(
+                  'ดำเนินต่อด้วยบัญชี Apple',
+                  style: TextStyle(fontSize: 15, color: Colors.black),
                 ),
               ),
-              child: const Text(
-                'ดำเนินต่อด้วยบัญชี Line',
-                style: TextStyle(fontSize: 15, color: Colors.black),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 105, vertical: 15), // การเว้นระยะในปุ่ม
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              child: const Text(
-                'ดำเนินต่อด้วยบัญชี Apple',
-                style: TextStyle(fontSize: 15, color: Colors.black),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
